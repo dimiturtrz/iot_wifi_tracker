e71f7922df75e8ed4f5dbc3bc74b3206981ae4a5
Dimitar <dimitar.trz@gmail.com>
2020-06-24 09:31:34 +0300

    rssi changes

--- wifi_manager/wifi_manager.ino
+++ wifi_manager/wifi_manager.ino
@@ -1,40 +1,91 @@
 #include <WiFiManager.h>
-#include <SPI.h>
 
-String testString;
+const char* logServer = "api.pushingbox.com";
 
-void setup() {
+String deviceId;
+String objectName;
 
-  Serial.begin(115200);
-  Serial.println("started");
+void setupWifiManager() {
   
   WiFiManager wifiManager;
   
-  // Uncomment and run it once, if you want to erase all the stored information
+  // In order to have the oportunity to set the devid (could be commented out)
   wifiManager.resetSettings();
   
-  // set custom ip for portal
+  // to be certain where to look
   wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
 
-  WiFiManagerParameter custom_string("test string", "enter test string", "", 40);
-  wifiManager.addParameter(&custom_string);
+  WiFiManagerParameter deviceIdParameter("pushing box scenario device id", "enter device id", "", 40);
+  wifiManager.addParameter(&deviceIdParameter);
+  WiFiManagerParameter objectNameParameter("object name", "enter object name", "object", 40);
+  wifiManager.addParameter(&objectNameParameter);
 
-  // fetches ssid and pass from eeprom and tries to connect
-  // if it does not connect it starts an access point with the specified name
-  // here  "AutoConnectAP"
-  // and goes into a blocking loop awaiting configuration
-  wifiManager.autoConnect("AutoConnectAP", "securePassword");
+  wifiManager.autoConnect("ESP8622TrackerAP", "securePassword");
 
-  testString = custom_string.getValue();
+  deviceId = deviceIdParameter.getValue();
+  objectName = objectNameParameter.getValue();
 }
+
+void sendNotification(){
+  
+  Serial.println();
+  Serial.println("- starting client");
+  
+  WiFiClient client;
+
+  Serial.println("- connecting to pushing server: " + String(logServer));
+
+  while(!client.connect(logServer, 80)) {
+    Serial.println("- problem connecting");
+  }
+
+  Serial.println("- succesfully connected");
+
+  String getString = "GET /pushingbox?devid=" + 
+                     String(deviceId) + 
+                     "&object_name=" + 
+                     String(objectName) + 
+                     " HTTP/1.1";
+  
+  Serial.println(getString);
+  Serial.println("- sending data...");
+
+  client.println(getString);
+  client.println(String("Host: ") + logServer);
+  client.println("Connection: close \n");
+
+  Serial.println("[Response:]");
+  while (client.connected() || client.available())
+  {
+    if (client.available())
+    {
+      String line = client.readStringUntil('\n');
+      Serial.println(line);
+    }
+  }
+  
+  client.stop();
+  Serial.println("- stopping the client");
+}
+
+void setup() {
+  Serial.begin(115200);
+  setupWifiManager();
+}
+
+
+boolean sentNotification = false;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(testString);
+  Serial.println(objectName);
   long rssi = WiFi.RSSI();
   Serial.print("RSSI:");
   Serial.println(rssi);
-  
-  
+  if(!sentNotification /*&& rssi < -80*/ ) {
+    Serial.println("sending");
+    sendNotification();
+    sentNotification = true;
+  }
   delay(10000);
 }
